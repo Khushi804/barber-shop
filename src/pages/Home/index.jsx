2dcaeb9d1c75f5263f3096cb4825b08b725cf922
Consoling <shahidarizfannymagnet@gmail.com>
2024-02-29 17:15:23 +0530

    second commit - 'File structure and basic components created'

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/index'
+import Banner from '../../components/Banner/index'
+const Home = () => {
+  return (
+    <>
+    <Navbar />
+    <Banner />
+    </>
+  )
+}
+
+export default Home
