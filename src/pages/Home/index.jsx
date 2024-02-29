cf505f7847b00c5e20fa321558ecde89f318db8b
Consoling <shahidarizfannymagnet@gmail.com>
2024-02-29 17:17:13 +0530

    second commit - 'component update'

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/index'
 import Banner from '../../components/Banner/index'
+import Hero from '../../components/Hero/index'
+import Footer from '../../components/Footer/index'
+
 const Home = () => {
   return (
     <>
     <Navbar />
     <Banner />
+    <Hero />
+    <Footer />
     </>
   )
 }
