c77bbdb0f56ee1c0b98b84a77dd8567d16d369ff
Khushi804 <khushigupta24680@gmail.com>
2024-03-01 16:33:07 +0530

    modified: Navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,35 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='Main-nav'>
+      <div className='Logo'>
+        <h2>
+          <span>B</span>arber
+          <span>S</span>hop
+        </h2>
+      </div>
+      <div className="menu-items">
+        <ul>
+          <li  >
+          <a href="#">HOME</a>
+          </li>
+          <li  >
+          <a href="#">ABOUT US </a>
+          </li>
+          <li  >
+          <a href="#">OUR SERVICES</a>
+          </li>
+          <li  >
+          <a href="#">CONTACT US </a>
+          </li>
+        </ul>
+      </div>
+      <div className="login-button">
+        <input type='button'>
+           Login
+        </input>
+      </div>
+    </nav>
   )
 }
 
