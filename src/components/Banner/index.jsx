4fb196617501e4fc7f8f1e5b5d93468e892690dd
Khushi804 <khushigupta24680@gmail.com>
2024-03-01 23:19:01 +0530

    modified Navbar

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import './Banner.css'
-
-const Banner = () => {
+import { Link } from 'react-router-dom';
+function Banner() {
   return (
+    
+ 
     <section className="info">
       <p>Partner with us for transformation impacts and effective solutions.Your success is our committment </p>
       <div>
-        <button className="callus "> 
-         CALL US
+        <button className="callus ">
+          CALL US
         </button>
 
       </div>
       <div>
-        <button className="enquiry "> 
-         ENQUIRY NOW
+        <button className="enquiry ">
+          ENQUIRY NOW
         </button>
 
       </div>
     </section>
+    
   )
 }
 
